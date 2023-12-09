60c0ef1d4c2a3329ff0c65a7c204cf794a6d1e7e
NEREA GOROSTIDI GARCIA <100496438@alumnos.uc3m.es>
2023-12-09 14:47:06 +0100

    Adding Comments everywhere

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+// Variables to keep track of the game state
 let strokes = 0;
 let emptyPiece;
 let dragStartIndex;
 let dragOverIndex;
 let moves = [];
 
+// Function to update the stroke counter displayed on the webpage
 function updateStrokeCounter() {
   document.getElementById("stroke-counter").textContent = `Strokes: ${strokes}`;
 }
 
+// Event handler for selecting an image file to create the puzzle
 function handleFileSelect(event) {
   const file = event.target.files[0];
 
@@ -26,17 +29,20 @@ function handleFileSelect(event) {
   }
 }
 
+// Function to create the puzzle grid based on the selected image
 function createPuzzle(image) {
+  // Configuration for the puzzle grid
   const numColsRows = 4; // Number of columns and rows in the puzzle
-
   const pieceWidth = image.width / numColsRows;
   const pieceHeight = image.height / numColsRows;
 
+  // Clear the puzzle container and set its grid properties
   const puzzleContainer = document.getElementById("puzzle-container");
   puzzleContainer.innerHTML = "";
   puzzleContainer.style.gridTemplateColumns = `repeat(${numColsRows}, 1fr)`;
   puzzleContainer.style.gridTemplateRows = `repeat(${numColsRows}, 1fr)`;
 
+  // Create puzzle pieces, shuffle them, and add event listeners
   let pieces = createPieces(image, numColsRows, pieceWidth, pieceHeight);
   const indexs = shuffleArray(pieces);
   pieces.forEach((piece, index) => {
@@ -53,14 +59,18 @@ function createPuzzle(image) {
 
     puzzleContainer.appendChild(puzzlePiece);
   });
+
+  // Update stroke counter
   updateStrokeCounter();
 }
 
+// Function to create puzzle pieces from the image
 function createPieces(image, numColsRows, pieceWidth, pieceHeight) {
   const pieces = [];
 
   for (let y = 0; y < numColsRows; y++) {
     for (let x = 0; x < numColsRows; x++) {
+      // Create a canvas for each puzzle piece and draw a portion of the image on it
       const pieceCanvas = document.createElement("canvas");
       pieceCanvas.width = pieceWidth;
       pieceCanvas.height = pieceHeight;
@@ -86,6 +96,7 @@ function createPieces(image, numColsRows, pieceWidth, pieceHeight) {
   return pieces;
 }
 
+// Function to shuffle the array and return an array of swapped indices
 function shuffleArray(array) {
   // Create an array filled with integers from 0 to array.length - 1
   // This will represent the original sequence of indices
@@ -110,32 +121,38 @@ function shuffleArray(array) {
   return swappedIndices;
 }
 
+// Event handler for drag start on puzzle piece
 function handleDragStart(event) {
   dragStartIndex = findPieceIndex(event.target);
 }
 
+// Function to find the index of a puzzle piece
 function findPieceIndex(piece) {
   return Array.from(piece.parentNode.children).indexOf(piece);
 }
 
+// Event handler for drag over a puzzle piece
 function handleDragOver(event) {
   event.preventDefault();
   dragOverIndex = findPieceIndex(event.target);
 }
 
+// Event handler for drop on a puzzle piece
 function handleDrop(event) {
   event.preventDefault();
 
   const puzzleContainer = document.getElementById("puzzle-container");
   const puzzlePieces = puzzleContainer.getElementsByClassName("puzzle-piece");
+  
+  // Swap pieces and update the game state
   // We should only swap tiles if the receiving tile is not the empty piece
   if (puzzlePieces[dragOverIndex].classList.contains("empty-piece")) return;
-
   swapPieces(dragStartIndex, dragOverIndex, puzzlePieces);
-  strokes++;
-  updateStrokeCounter();
+  strokes++;   //Update strokes & Display strokes
+  updateStrokeCounter();  
   moves.push({ from: dragStartIndex, to: dragOverIndex }); // Record the move
 
+  // Check if the puzzle is solved
   checkPuzzle();
 }
 
@@ -164,6 +181,7 @@ function swapPieces(fromIndex, toIndex, puzzlePieces) {
   toPiece.dataset.currentIndex = fromCurrentIndex;
 }
 
+// Function to check if the puzzle is solved
 function checkPuzzle() {
   const puzzlePieces = document.querySelectorAll(".puzzle-piece");
   const correctOrder = Array.from(puzzlePieces).map((piece) =>
@@ -176,6 +194,7 @@ function checkPuzzle() {
   //   console.log(currentOrder);
   //   console.log(correctOrder);
 
+  // Check if the current order matches the correct order
   const solved = JSON.stringify(correctOrder) === JSON.stringify(currentOrder);
 
   if (solved) {
